e557bc1ff06bb42496eebe79fe92c28eebb05058
shaliniaiitd <=shalinia.iitd@gmail.com>
2023-12-12 22:09:36 +0530

    added tests for all 4 services

--- HT1.py
+++ HT1.py
@@ -10,15 +10,15 @@ apiurl = "http://www.dneonline.com//calculator.asmx"
 headers = {
     'Content-Type': 'text/xml; charset=utf-8',
 }
-
-def test_add():
+@pytest.mark.parametrize('int1, int2, result', [(4,4,'8'), (-2,-6,'-8'),(2,-6,'-4'),(5,-5,'0'),(2.5,5.6,'8.1')])
+def test_add(int1,int2,result):
     headers['SOAPAction'] = 'http://tempuri.org/Add'
-    soap1_input='''<?xml version="1.0" encoding="utf-8"?>
+    soap1_input=f'''<?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
       <soap:Body>
         <Add xmlns="http://tempuri.org/">
-          <intA>1</intA>
-          <intB>6</intB>
+          <intA>{int1}</intA>
+          <intB>{int2}</intB>
         </Add>
       </soap:Body>
     </soap:Envelope>'''
@@ -28,10 +28,10 @@ def test_add():
     xpars = xmltodict.parse(r1.text)
     print(xpars)
 
-    assert (xpars["soap:Envelope"]["soap:Body"]["AddResponse"]["AddResult"]) == '7'
+    assert (xpars["soap:Envelope"]["soap:Body"]["AddResponse"]["AddResult"]) == result
 
 #Test Divide
-@pytest.mark.parametrize('int1, int2, result', [(24,4,'6')])
+@pytest.mark.parametrize('int1, int2, result', [(24,4,'6'), (24,-6,'-4'),(12,6,'2')])
 def test_divide(int1,int2,result):
     headers['SOAPAction'] = 'http://tempuri.org/Divide'
 
@@ -52,28 +52,49 @@ def test_divide(int1,int2,result):
 
     assert (xpars["soap:Envelope"]["soap:Body"]["DivideResponse"]["DivideResult"]) == result
 
-# #Test Divide
-# headers = {
-# 'Content-Type': 'text/xml; charset=utf-8',
-# 'SOAPAction': 'http://tempuri.org/Divide'
-# }
-#
-# soap_input='''<?xml version="1.0" encoding="utf-8"?>
-# <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-#   <soap:Body>
-#     <Divide xmlns="http://tempuri.org/">
-#       <intA>24</intA>
-#       <intB>6</intB>
-#     </Divide>
-#   </soap:Body>
-# </soap:Envelope>'''
-#
-# r1 = requests.post(apiurl, headers=headers, data= soap_input)
-#
-# # print(r1.headers)
-# # print(r1.status_code)
-#
-# xpars = xmltodict.parse(r1.text)
-# print(xpars)
-#
-# assert (xpars["soap:Envelope"]["soap:Body"]["DivideResponse"]["DivideResult"]) == '4'
+    #One defect is division by Zero error is not addressed.
+
+#Test Multiply
+@pytest.mark.parametrize('int1, int2, result', [(6,4,'24'), (-3,-6,'18'),(-2,6,'-12'),(0,7,'0')])
+def test_multiply(int1,int2,result):
+    headers['SOAPAction'] = 'http://tempuri.org/Multiply'
+
+    soap_input=f'''<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <Multiply xmlns="http://tempuri.org/">
+          <intA>{int1}</intA>
+          <intB>{int2}</intB>
+        </Multiply>
+      </soap:Body>
+    </soap:Envelope>'''
+
+    r1 = requests.post(apiurl, headers=headers, data= soap_input)
+
+    xpars = xmltodict.parse(r1.text)
+    print(xpars)
+
+    assert (xpars["soap:Envelope"]["soap:Body"]["MultiplyResponse"]["MultiplyResult"]) == result
+
+#Test Subtract
+@pytest.mark.parametrize('int1, int2, result', [(6,4,'2'), (4,-6,'10'),(2,6,'-4'),(3,0,'3')])
+def test_subtract(int1,int2,result):
+    headers['SOAPAction'] = 'http://tempuri.org/Subtract'
+
+    soap_input=f'''<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <Subtract xmlns="http://tempuri.org/">
+          <intA>{int1}</intA>
+          <intB>{int2}</intB>
+        </Subtract>
+      </soap:Body>
+    </soap:Envelope>'''
+
+    r1 = requests.post(apiurl, headers=headers, data= soap_input)
+
+    xpars = xmltodict.parse(r1.text)
+    print(xpars)
+
+    assert (xpars["soap:Envelope"]["soap:Body"]["SubtractResponse"]["SubtractResult"]) == result
+
